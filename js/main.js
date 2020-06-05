8c406f821b86cfa13351784018e19bdd9cd11b90
ahmetys <66472682+ahmetys@users.noreply.github.com>
2020-06-05 13:50:53 +0200

    ilk proje

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("toggle-bar").addEventListener("click", () => {
+    document.getElementById("mobile-menu").style.display = "block";
+})
+document.getElementById("close-menu").addEventListener("click", () => {
+    document.getElementById("mobile-menu").style.display = "none";
+})
